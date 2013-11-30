2c334682d26092179e00584a9fad51e9b06a1be7
Hector Zarate <hecktorzr@gmail.com>
2013-12-01 00:47:52 +0100

    Enabled communication with app

--- Source-Code/NorthPole/src/NorthPole.c
+++ Source-Code/NorthPole/src/NorthPole.c
@@ -3,16 +3,43 @@
 static Window *window;
 static TextLayer *text_layer;
 
+static char altitude[16];
+
+enum AltitudeKey {
+    ALTITUDE_METERS_KEY = 0x0,  // TUPLE_CSTRING
+};
+
+static AppSync sync;
+static uint8_t sync_buffer[64];
+
+
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context)
+{
+    switch (key)
+    {
+        default:
+        case ALTITUDE_METERS_KEY:
+        {
+            text_layer_set_text(text_layer, new_tuple->value->cstring);
+        }
+    }
+}
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+  
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+//  text_layer_set_text(text_layer, "Up");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+//  text_layer_set_text(text_layer, "Down");
 }
 
 static void click_config_provider(void *context) {
@@ -26,12 +53,25 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+    
+    text_layer_set_text_color(text_layer, GColorWhite);
+    text_layer_set_background_color(text_layer, GColorBlack);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+    
+    
+    Tuplet initial_values[] = {
+        TupletCString(ALTITUDE_METERS_KEY, "0.0 m"),
+    };
+    
+    app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
+                  sync_tuple_changed_callback, sync_error_callback, NULL);
+    
+    
 }
 
 static void window_unload(Window *window) {
+  app_sync_deinit(&sync);
   text_layer_destroy(text_layer);
 }
 
@@ -42,6 +82,12 @@ static void init(void) {
     .load = window_load,
     .unload = window_unload,
   });
+    
+    const int inbound_size = 64;
+    const int outbound_size = 16;
+    app_message_open(inbound_size, outbound_size);
+    
+    
   const bool animated = true;
   window_stack_push(window, animated);
 }
