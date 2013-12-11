dc3c932e75ee59016aafda43b5c102a9d6bc0dc8
Hector Zarate <hecktorzr@gmail.com>
2013-12-11 15:43:22 +0100

    Added function to send direction abbreviation

--- Source-Code/NorthPole/src/js/pebble-js-app.js
+++ Source-Code/NorthPole/src/js/pebble-js-app.js
@@ -1,12 +1,32 @@
+function abbreviationForDirection(direction)
+{
+    var allAbbreviations = ["N", "NW", "W", "SW", "S", "SE", "E", "NE"];
+
+    var widthForPointInDegrees = 360.0 / allAbbreviations.length;
+
+    direction = direction + (360 + (widthForPointInDegrees / 2.0));
+	                    	    	                                
+    direction = direction % 360;
+	                    	    	                                        
+    var position = direction / widthForPointInDegrees;
+	                    	    	                                                
+    return allAbbreviations[position];
+}
+
+
+function getCurrentPosition()
+{
+    window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+}
 
 function locationSuccess(pos) {
 	var coordinates = pos.coords;
-
-
+    
+    var directionKey = abbreviationForDirection(coordinates.heading);
 
 	Pebble.sendAppMessage({
 		"altitude":coordinates.altitude,
-        "direction":"NA",
+        "direction":directionKey,
         "directionDegrees":coordinates.heading
     });
 }
@@ -25,10 +45,6 @@ Pebble.addEventListener("ready",
     function(e) {
         locationWatcher = window.navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
   	    console.log("JavaScript app ready and running!");
+        setInterval(function(){ getCurrentPosition() }, 1000 / 9);
   }
  );
-
-//Pebble.addEventListener("appmessage",
-//                        function(e) {
-//        window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
-//                        });
