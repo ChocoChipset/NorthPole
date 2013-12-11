4f52a9c5120b75c4a0fbffb96a35ccc185be98ee
Hector Zarate <hecktorzr@gmail.com>
2013-12-11 15:11:05 +0100

    Javascript application, first communication with watch

--- Source-Code/NorthPole/src/js/pebble-js-app.js
+++ Source-Code/NorthPole/src/js/pebble-js-app.js
@@ -1,5 +1,34 @@
+
+function locationSuccess(pos) {
+	var coordinates = pos.coords;
+
+
+
+	Pebble.sendAppMessage({
+		"altitude":coordinates.altitude,
+        "direction":"NA",
+        "directionDegrees":coordinates.heading
+    });
+}
+
+function locationError(err) {
+	Pebble.sendAppMessage({
+		"altitude":0.0,
+		"direction":"NA"
+		"directionDegrees":0.0
+    });
+}
+
+var locationOptions = { "timeout": 15000, "maximumAge": 60000 };
+
 Pebble.addEventListener("ready",
-  function(e) {
-  	  console.log("JavaScript app ready and running!");
+    function(e) {
+        locationWatcher = window.navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
+  	    console.log("JavaScript app ready and running!");
   }
  );
+
+//Pebble.addEventListener("appmessage",
+//                        function(e) {
+//        window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+//                        });
